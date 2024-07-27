d0ea6b256a79870a2299db6508d6e039456f722f
Felipe R. <feliperamos0424@hotmail.com>
2024-07-27 17:41:48 -0300

    Atualização Portfólio

---
+++ src/public/JS/TESTE.js
@@ -0,0 +1,12 @@
+var quadrado = {
+    lado: 4,
+    area: function () {
+        return this.lado * this.lado;
+    },
+    perimetro: function (){
+        return this.lado * this.lado;
+    }
+}
+
+console.log(quadrado.perimetro());
+console.log(quadrado.area());
